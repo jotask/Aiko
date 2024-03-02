e2d6461534e71f16e0a80dad4748aed522042158
Jose <j.iznardo@double11.com>
2024-03-02 20:53:08 +0000

    remove debug code

--- aiko/src/systems/render_system.cpp
+++ aiko/src/systems/render_system.cpp
@@ -91,24 +91,9 @@ namespace aiko
 
     void RenderSystem::render(texture::RenderTexture2D& target, shader::Shader& shader)
     {
-        {
-            m_renderModule->beginShaderMode(&shader);
-            m_renderModule->drawTexture(target.texture, 0, 0, WHITE);
-            m_renderModule->endShaderMode();
-        }
-
-        {
-            m_renderModule->beginTextureMode(target);
-            m_renderModule->drawRectangle(0.0f, 0.0f, 100.f, 100.f, RED);
-            m_renderModule->endTextureMode();
-        }
-
-        {
-            m_renderModule->beginTextureMode(target);
-            m_renderModule->drawCircle(300.f, 250.f, 100.f, SKYBLUE);
-            m_renderModule->endTextureMode();
-        }
-
+        m_renderModule->beginShaderMode(&shader);
+        m_renderModule->drawTexture(target.texture, 0, 0, WHITE);
+        m_renderModule->endShaderMode();
     }
 
 }
