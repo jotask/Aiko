11fc6d32f2fa42db16bf96c6e5226fdcfa287c0a
Jose <j.iznardo@double11.com>
2024-03-28 22:16:01 +0000

    adding assimp to project

--- aiko/Dependencies.cmake
+++ aiko/Dependencies.cmake
@@ -43,6 +43,23 @@ endif()
 #----------------------------------------------------------------------
 
 FetchContent_Declare(
+    assimp
+    GIT_REPOSITORY https://github.com/assimp/assimp.git
+    GIT_TAG        v5.3.1
+    GIT_SHALLOW    TRUE
+    GIT_PROGRESS   TRUE
+)
+message("Fetching assimp")
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
+set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(ASSIMP_INJECT_DEBUG_POSTFIX OFF CACHE BOOL "" FORCE)
+set(ASSIMP_INSTALL OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(assimp)
+
+#----------------------------------------------------------------------
+
+FetchContent_Declare(
     spdlog
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
     GIT_TAG        v1.10.0
