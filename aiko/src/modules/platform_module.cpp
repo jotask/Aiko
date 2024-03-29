bd7c5cb4570992ba42dbc66c5bd845a0eddae40d
Jose <j.iznardo@double11.com>
2024-03-29 19:46:40 +0000

    update time on platform module

--- aiko/src/modules/platform_module.cpp
+++ aiko/src/modules/platform_module.cpp
@@ -7,6 +7,7 @@
 
 #include "core/libs.h"
 #include "models/time.h"
+#include <GLFW/glfw3.h>
 
 namespace aiko
 {
@@ -23,10 +24,34 @@ namespace aiko
 
     void PlatformModule::updateTime()
     {
+        // Delta Time
+        static double currentFrame = glfwGetTime();
+        static double lastFrame = currentFrame;
+        static double deltaTime;
+
+        // Fps
+        static double previousTime = glfwGetTime();
+        static int frameCount = 0;
+
+        currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         auto& time = Time::it();
-        // time.deltaTime = ::GetFrameTime();
-        // time.fps = ::GetFPS();
+        time.deltaTime = deltaTime;
         time.frames++;
+
+        frameCount++;
+        // If a second has passed.
+        if (currentFrame - previousTime >= 1.0)
+        {
+            // Display the frame count here any way you want.
+            time.fps = frameCount;
+
+            frameCount = 0;
+            previousTime = currentFrame;
+        }
+
     }
 
 }
