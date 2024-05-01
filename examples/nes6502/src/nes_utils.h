dd2829d1530a4eee8371b042d46ebb8df165b3af
Jose <j.iznardo@double11.com>
2024-05-01 22:47:57 +0100

    adding nes utils to convert bytes to strings or other functions

---
+++ examples/nes6502/src/nes_utils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include  <string>
+#include <iomanip>
+#include <sstream>
+
+#include "nes_types.h"
+
+namespace nes
+{
+    std::string toString(Byte value)
+    {
+        static std::string buffer;
+        static std::stringstream ss;
+        ss.str("");
+        ss << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(value);
+        return ss.str();
+    }
+
+}
