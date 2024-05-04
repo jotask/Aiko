c60110da7e630cf7db8766db34e75bdfa4357ad5
Jose <j.iznardo@double11.com>
2024-05-04 22:17:25 +0100

    adding support for a few more addressing modes

--- examples/nes6502/src/cpu_addressing_modes.cpp
+++ examples/nes6502/src/cpu_addressing_modes.cpp
@@ -93,11 +93,22 @@ namespace nes
     void Cpu::absoluteX()
     {
         m_currentAddressMode = AddressModes::AbsoluteX;
+        Memory* mem = getMemory(bus);
+        Byte high = mem->read(program_counter++);
+        Byte low = mem->read(program_counter++);
+        Word absoluteMemory = toWord(high, low) + X;
+        memoryFetched = mem->read(absoluteMemory);
+        int a = 0;
     }
 
     void Cpu::absoluteY()
     {
         m_currentAddressMode = AddressModes::AbsoluteY;
+        Memory* mem = getMemory(bus);
+        Byte high = mem->read(program_counter++);
+        Byte low = mem->read(program_counter++);
+        Word absoluteMemory = toWord(high, low) + Y;
+        memoryFetched = mem->read(absoluteMemory);
     }
 
     void Cpu::indirect()
@@ -108,11 +119,60 @@ namespace nes
     void Cpu::indirectX()
     {
         m_currentAddressMode = AddressModes::IndirectX;
+        Memory* mem = getMemory(bus);
+        // Fetch the zero page address from memory and increment the program counter
+        Byte zeropageAddress = mem->read(program_counter++);
+        // Calculate the effective address by adding the X register to the zero page address
+        Word effectiveAddress = zeropageAddress + X;
+        // Read the low of the target address from the effective address
+        Byte msb = mem->read(effectiveAddress);
+
+        // Increment the effective address to read the MSB of the target address
+        effectiveAddress++;
+
+        // Ensure correct wrap-around for zero page addressing
+        if ((effectiveAddress & 0xFF) == 0) {
+            effectiveAddress = (effectiveAddress & 0xFF00) | (zeropageAddress & 0xFF);
+        }
+
+        // Read the MSB of the target address from the adjusted effective address
+        Byte lsb = mem->read(effectiveAddress);
+
+        // Combine the LSB and MSB to form the target address
+        Word targetAddress = nes::toWord(msb, lsb);;
+
+        // Read the byte from memory at the target address
+        memoryFetched = mem->read(targetAddress);
+
     }
 
     void Cpu::indirectY()
     {
         m_currentAddressMode = AddressModes::IndirectY;
+        Memory* mem = getMemory(bus);
+        // Fetch the zero page address from memory and increment the program counter
+        Byte zeropageAddress = mem->read(program_counter++);
+        // Calculate the effective address by adding the X register to the zero page address
+        Word effectiveAddress = zeropageAddress + Y;
+        // Read the low of the target address from the effective address
+        Byte msb = mem->read(effectiveAddress);
+
+        // Increment the effective address to read the MSB of the target address
+        effectiveAddress++;
+
+        // Ensure correct wrap-around for zero page addressing
+        if ((effectiveAddress & 0xFF) == 0) {
+            effectiveAddress = (effectiveAddress & 0xFF00) | (zeropageAddress & 0xFF);
+        }
+
+        // Read the MSB of the target address from the adjusted effective address
+        Byte lsb = mem->read(effectiveAddress);
+
+        // Combine the LSB and MSB to form the target address
+        Word targetAddress = nes::toWord(msb, lsb);;
+
+        // Read the byte from memory at the target address
+        memoryFetched = mem->read(targetAddress);
     }
 
 }
