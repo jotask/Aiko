44d35fb0015bcc760099ff6f5cfc5b75d728ef59
Jose <j.iznardo@double11.com>
2024-05-04 01:08:40 +0100

    adding isntructions enums

--- examples/nes6502/src/instructions.h
+++ examples/nes6502/src/instructions.h
@@ -1,15 +1,48 @@
 #pragma once
 
+#include <exception>
+
 #include "nes_types.h"
 #include "microprocessor.h"
 
 namespace nes
 {
 
-    class Instruction
+    enum class AddressModes
     {
-        Byte opCode;
-        u32 cycles;
+        Implied,
+        Immediate,
+        ZeroPage,
+        ZeroPageX,
+        ZeroPageY,
+        Absolute,
+        AbsoluteX,
+        AbsoluteY,
+        Indirect,
+        IndirectX,
+        IndirectY,
+        Relative,
+        Inmediate
     };
+
+    static const char* to_string(AddressModes mode)
+    {
+        switch (mode)
+        {
+        case AddressModes::Implied:     return"Implied";
+        case AddressModes::Immediate:   return"Immediate";
+        case AddressModes::ZeroPage:    return"ZeroPage";
+        case AddressModes::ZeroPageX:   return"ZeroPageX";
+        case AddressModes::ZeroPageY:   return"ZeroPageY";
+        case AddressModes::Absolute:    return"Absolute";
+        case AddressModes::AbsoluteX:   return"AbsoluteX";
+        case AddressModes::AbsoluteY:   return"AbsoluteY";
+        case AddressModes::Indirect:    return"Indirect";
+        case AddressModes::IndirectX:   return"IndirectX";
+        case AddressModes::IndirectY:   return"IndirectY";
+        case AddressModes::Relative:    return"Relative";
+        default:                        std::exception("Addressing Mode Unknow");
+        }
+    }
 
 }
