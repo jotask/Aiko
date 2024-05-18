6e1db13e7345b635857a900faf8f870571b71428
Jose <josevivesiznardo@gmail.com>
2024-05-18 13:38:51 +0200

    disable some json configurations

--- examples/nes6502/Dependencies.cmake
+++ examples/nes6502/Dependencies.cmake
@@ -5,6 +5,13 @@ Set(FETCHCONTENT_QUIET FALSE)
 
 #----------------------------------------------------------------------
 
+set(JSONCPP_WITH_TESTS OFF CACHE BOOL "" FORCE)
+set(JSONCPP_WITH_POST_BUILD_UNITTEST OFF CACHE BOOL "" FORCE)
+set(JSONCPP_WITH_WARNING_AS_ERROR OFF CACHE BOOL "" FORCE)
+set(JSONCPP_WITH_EXAMPLE OFF CACHE BOOL "" FORCE)
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
+set(BUILD_OBJECT_LIBS OFF CACHE BOOL "" FORCE)
+
 FetchContent_Declare(
     jsoncpp
     GIT_REPOSITORY https://github.com/open-source-parsers/jsoncpp.git
