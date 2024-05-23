c3b237e1dbf62d5c51085fdf1333a89d2502818a
Jose <j.iznardo@double11.com>
2024-05-23 22:08:05 +0100

    adding preprocessor to disable test cubes

--- examples/sandbox/src/sandbox.cpp
+++ examples/sandbox/src/sandbox.cpp
@@ -10,6 +10,8 @@
 #include "modules/render_primitives.h"
 #include "models/camera.h"
 
+#define TEST_CUBES
+
 namespace sandbox
 {
     void Sandbox::init()
@@ -18,27 +20,32 @@ namespace sandbox
 
         getMainCamera()->setCameraController(camera::CameraController::Orbit);
 
+#ifdef TEST_CUBES
         m_go1 = this->createGameObject("Cube1");
         m_go1->transform()->position = { 1.0f, 0.0f, 0.0f };
         m_go1->transform()->rotation = { 0.0f, 0.0f, 0.0f };
         m_go1->transform()->scale = { 1.0f, 1.0f, 1.0f };
         auto mesh1 = m_go1->addComponent<aiko::MeshComponent>();
-
+        
         m_go2 = this->createGameObject("Cube2");
         m_go2->transform()->position = { -1.0f, 0.0f, 0.0f };
         m_go2->transform()->rotation = { 0.0f, 0.0f, 0.0f };
         m_go2->transform()->scale = { 1.0f, 1.0f, 1.0f };
         auto mesh2 = m_go2->addComponent<aiko::MeshComponent>();
+#endif
+
     }
 
     void Sandbox::update()
     {
         Application::update();
+#ifdef TEST_CUBES
         static float angle = 0.0f;
         angle += 10.0f * getlDeltaTime();
         angle = fmod(angle, 360.0f);
         m_go1->transform()->rotation = { angle, 0.0f, 0.0f };
         m_go2->transform()->rotation = { 0.0f, 0.0f, 0.0f };
+#endif
     }
 
     void Sandbox::render()
