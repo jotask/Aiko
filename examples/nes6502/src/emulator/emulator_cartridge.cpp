e7c68c30cab7c8df39d8ffc78af9f7826a978ebb
Jose <j.iznardo@double11.com>
2024-05-25 22:05:11 +0100

    rendering CHR table

--- examples/nes6502/src/emulator/emulator_cartridge.cpp
+++ examples/nes6502/src/emulator/emulator_cartridge.cpp
@@ -16,6 +16,16 @@ namespace nes
 
     void CartridgeWindow::update()
     {
+        static bool updated = false;
+        if (updated == false)
+        {
+            updated = true;
+            Cartridge* cart = naiko->getCartridge();
+            auto pixels = convertPatternTableToTexture(cart->getCHR());
+            auto* pbo = naiko->getApplication()->getPT0();
+            pbo->updatePixels(pixels);
+            pbo->refreshPixels();
+        }
 
     }
 
@@ -23,47 +33,41 @@ namespace nes
     {
         if (ImGui::Begin(name.c_str(), &is_open))
         {
-
-            Cartridge* cart = naiko->getCartridge();
-            auto pixels = convertPatternTableToTexture(cart->getCHR());
-
-            auto* pbo = naiko->getApplication()->getPT0();
-            pbo->updatePixels(pixels);
-            pbo->refreshPixels();
-
             ImGui::BeginChild("CHR table");
-            // Get the size of the child (i.e. the whole draw size of the windows).
-            ImVec2 wsize = ImGui::GetWindowSize();
-            // Because I use the texture from OpenGL, I need to invert the V from the UV.
-            ImGui::Image((ImTextureID)pbo->getPboTexture().texture.id, wsize, ImVec2(0, 1), ImVec2(1, 0));
+            auto* pbo = naiko->getApplication()->getPT0();
+            ImVec2 wsize = ImVec2(pbo->getPboTexture().texture.width, pbo->getPboTexture().texture.height);
+            ImGui::Image((ImTextureID)(intptr_t)pbo->getPboTexture().texture.id, wsize, ImVec2(0, 1), ImVec2(1, 0));
             ImGui::EndChild();
-
         }
         ImGui::End();
     }
 
     std::vector<aiko::Color> CartridgeWindow::convertPatternTableToTexture(const std::vector<Byte>& patternTable)
     {
-        constexpr uint16_t size = 128 * 256;
-        std::vector<aiko::Color> textureData(size);
+        constexpr Word width = 128;
+        constexpr Word height = 256;
+        std::vector<aiko::Color> textureData(width * height);
 
-        for (int tileY = 0; tileY < 16; ++tileY)
+        for (int table = 0; table < 2; ++table)
         {
-            for (int tileX = 0; tileX < 16; ++tileX)
+            for (int tileY = 0; tileY < 16; ++tileY)
             {
-                for (int row = 0; row < 8; ++row)
+                for (int tileX = 0; tileX < 16; ++tileX)
                 {
-
-                    Byte plane0 = patternTable[tileY * 256 + tileX * 16 + row];
-                    Byte plane1 = patternTable[tileY * 256 + tileX * 16 + row + 8];
-
-                    for (int col = 0; col < 8; ++col) 
+                    for (int row = 0; row < 8; ++row)
                     {
 
-                        Byte pixelValue = ((plane1 >> (7 - col)) & 1) << 1 | ((plane0 >> (7 - col)) & 1);
-                        float color = pixelValue * 85; // Simple grayscale based on pixel value
-                        int texIndex = (tileY * 8 + row) * 256 + (tileX * 8 + col);
-                        textureData[texIndex] = { color, color, color, 1.0f };
+                        int baseIndex = table * 4096 + tileY * 256 + tileX * 16 + row;
+                        Byte plane0 = patternTable[baseIndex];
+                        Byte plane1 = patternTable[baseIndex + 8];
+
+                        for (int col = 0; col < 8; ++col)
+                        {
+                            Byte pixelValue = ((plane1 >> (7 - col)) & 1) << 1 | ((plane0 >> (7 - col)) & 1);
+                            float color = static_cast<float>(pixelValue);
+                            int texIndex = (table * 128 * 128) + (tileY * 8 + row) * 128 + (tileX * 8 + col);
+                            textureData[texIndex] = { color, color, color, 1.0f };
+                        }
                     }
                 }
             }
