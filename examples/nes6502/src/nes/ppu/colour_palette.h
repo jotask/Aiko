ab7246993025dffcb1187ca2459e2ffe4ea655c7
Jose <j.iznardo@double11.com>
2024-05-25 23:26:14 +0100

    extracting color palette to his own file

---
+++ examples/nes6502/src/nes/ppu/colour_palette.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <assert.h>
+
+#include <aiko_includes.h>
+#include "nes/nes_types.h"
+
+namespace nes
+{
+    namespace palette
+    {
+        static const aiko::Color colour_palette[COLOUR_PALETTE_SIZE]
+        {
+            aiko::Color(84, 84, 84),
+            aiko::Color(0, 30, 116),
+            aiko::Color(8, 16, 144),
+            aiko::Color(48, 0, 136),
+            aiko::Color(68, 0, 100),
+            aiko::Color(92, 0, 48),
+            aiko::Color(84, 4, 0),
+            aiko::Color(60, 24, 0),
+            aiko::Color(32, 42, 0),
+            aiko::Color(8, 58, 0),
+            aiko::Color(0, 64, 0),
+            aiko::Color(0, 60, 0),
+            aiko::Color(0, 50, 60),
+            aiko::Color(0, 0, 0),
+            aiko::Color(0, 0, 0),
+            aiko::Color(0, 0, 0),
+            aiko::Color(152, 150, 152),
+            aiko::Color(8, 76, 196),
+            aiko::Color(48, 50, 236),
+            aiko::Color(92, 30, 228),
+            aiko::Color(136, 20, 176),
+            aiko::Color(160, 20, 100),
+            aiko::Color(152, 34, 32),
+            aiko::Color(120, 60, 0),
+            aiko::Color(84, 90, 0),
+            aiko::Color(40, 114, 0),
+            aiko::Color(8, 124, 0),
+            aiko::Color(0, 118, 40),
+            aiko::Color(0, 102, 120),
+            aiko::Color(0, 0, 0),
+            aiko::Color(0, 0, 0),
+            aiko::Color(0, 0, 0),
+            aiko::Color(236, 238, 236),
+            aiko::Color(76, 154, 236),
+            aiko::Color(120, 124, 236),
+            aiko::Color(176, 98, 236),
+            aiko::Color(228, 84, 236),
+            aiko::Color(236, 88, 180),
+            aiko::Color(236, 106, 100),
+            aiko::Color(212, 136, 32),
+            aiko::Color(160, 170, 0),
+            aiko::Color(116, 196, 0),
+            aiko::Color(76, 208, 32),
+            aiko::Color(56, 204, 108),
+            aiko::Color(56, 180, 204),
+            aiko::Color(60, 60, 60),
+            aiko::Color(0, 0, 0),
+            aiko::Color(0, 0, 0),
+            aiko::Color(236, 238, 236),
+            aiko::Color(168, 204, 236),
+            aiko::Color(188, 188, 236),
+            aiko::Color(212, 178, 236),
+            aiko::Color(236, 174, 236),
+            aiko::Color(236, 174, 212),
+            aiko::Color(236, 180, 176),
+            aiko::Color(228, 196, 144),
+            aiko::Color(204, 210, 120),
+            aiko::Color(180, 222, 120),
+            aiko::Color(168, 226, 144),
+            aiko::Color(152, 226, 180),
+            aiko::Color(160, 214, 228),
+            aiko::Color(160, 162, 160),
+            aiko::Color(0, 0, 0),
+            aiko::Color(0, 0, 0),
+        };
+
+    }
+}
