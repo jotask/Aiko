ee1dc9d3ce0d4e14fc258cb2585a4d686becc918
Jose <j.iznardo@double11.com>
2024-05-25 23:59:03 +0100

    static size draw colour palette

--- examples/nes6502/src/emulator/emulator_ppu.cpp
+++ examples/nes6502/src/emulator/emulator_ppu.cpp
@@ -36,40 +36,13 @@ namespace nes
     {
         if (ImGui::Begin(name.c_str(), &is_open))
         {
+            ImGui::Text("Colour Palette");
             ImGui::BeginChild("Palette Color");
             auto* pbo = naiko->getApplication()->getPalette();
             // Get the dimensions of the texture
-            ImVec2 textureSize = ImVec2(pbo->getPboTexture().texture.width, pbo->getPboTexture().texture.height);
-
-            // Calculate aspect ratio of the image
-            float aspectRatio = textureSize.x / textureSize.y;
-
-            // Get available space in the window
-            ImVec2 availableSpace = ImGui::GetContentRegionAvail();
-
-            // Calculate the maximum size of the image while maintaining the aspect ratio
-            float maxWidth = availableSpace.x;
-            float maxHeight = availableSpace.y;
-            float imageWidth, imageHeight;
-
-            if (maxWidth / maxHeight > aspectRatio)
-            {
-                // Height is the limiting factor
-                imageHeight = maxHeight;
-                imageWidth = maxHeight * aspectRatio;
-            }
-            else
-            {
-                // Width is the limiting factor
-                imageWidth = maxWidth;
-                imageHeight = maxWidth / aspectRatio;
-            }
-
-            // Ensure that the image does not exceed the available space
-            imageWidth = std::min(imageWidth, maxWidth);
-            imageHeight = std::min(imageHeight, maxHeight);
-
-            ImGui::Image((ImTextureID)pbo->getPboTexture().texture.id, ImVec2(imageWidth, imageHeight), ImVec2(0, 0), ImVec2(1, 1));
+            constexpr const float size_multiplier = 16;
+            ImVec2 textureSize = ImVec2(pbo->getPboTexture().texture.width * size_multiplier, pbo->getPboTexture().texture.height * size_multiplier);
+            ImGui::Image((ImTextureID)pbo->getPboTexture().texture.id, textureSize, ImVec2(0, 0), ImVec2(1, 1));
             ImGui::EndChild();
         }
         ImGui::End();
