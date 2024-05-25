70d8767bd974d1fa8ae8ce849de3a3c579dbd001
Jose <j.iznardo@double11.com>
2024-05-25 23:26:28 +0100

    adding color constructor so we can default alpha

--- aiko/src/types/color.h
+++ aiko/src/types/color.h
@@ -11,6 +11,7 @@ namespace aiko
         Color() : Color(0.0f, 1.0f) { };
         Color(float rgb, float a) : Color(rgb, rgb, rgb, a) { };
         Color(float rgba) : Color(rgba, rgba, rgba, rgba) { };
+        Color(float r, float g, float b) : r(r), g(g), b(b), a(1.0f) { };
         Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) { };
         float r;
         float g;
