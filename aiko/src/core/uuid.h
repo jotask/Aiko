2b4f771645d31c8239c56aaf5dc168dd8cb4e4d8
Jose <j.iznardo@double11.com>
2024-05-27 23:25:31 +0100

    improve uuid

--- aiko/src/core/uuid.h
+++ aiko/src/core/uuid.h
@@ -20,9 +20,10 @@ namespace uuid
 
         }
 
-        const char* operator& ()
+        Uuid(std::string uuid)
+            : m_uuid(uuid)
         {
-            return m_uuid.c_str();
+
         }
 
         const std::string get() const
@@ -30,31 +31,42 @@ namespace uuid
             return m_uuid;
         }
 
-    private:
-        const std::string m_uuid;
+        std::string& operator&()
+        {
+            return m_uuid;
+        }
 
-        std::string generate_uuid_v4() {
+    private:
+        std::string m_uuid;
+
+        std::string generate_uuid_v4()
+        {
             std::stringstream ss;
             int i;
             ss << std::hex;
-            for (i = 0; i < 8; i++) {
+            for (i = 0; i < 8; i++)
+            {
                 ss << dis(gen);
             }
             ss << "-";
-            for (i = 0; i < 4; i++) {
+            for (i = 0; i < 4; i++)
+            {
                 ss << dis(gen);
             }
             ss << "-4";
-            for (i = 0; i < 3; i++) {
+            for (i = 0; i < 3; i++)
+            {
                 ss << dis(gen);
             }
             ss << "-";
             ss << dis2(gen);
-            for (i = 0; i < 3; i++) {
+            for (i = 0; i < 3; i++)
+            {
                 ss << dis(gen);
             }
             ss << "-";
-            for (i = 0; i < 12; i++) {
+            for (i = 0; i < 12; i++)
+            {
                 ss << dis(gen);
             };
             return ss.str();
