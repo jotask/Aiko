4a86bba390b748e4274bacc3cc8da57217ee6c1a
Jose <j.iznardo@double11.com>
2024-09-15 20:31:16 +0100

    update test data

--- aiko/src/systems/render_system.cpp
+++ aiko/src/systems/render_system.cpp
@@ -43,10 +43,10 @@ namespace aiko
     {
         auto ctx = m_renderModule->getRenderer<RenderContext3D>(ContextType::Render3D);
         assert(ctx != nullptr);
-        ctx->drawRectangle(getMainCamera(), { 000.0f, 000.0f }, { 100.0f, 100.0f }, GREEN);
+        ctx->drawRectangle(getMainCamera(), { 000.0f, 000.0f }, { 100.0f, 100.0f }, RED);
         ctx->drawRectangle(getMainCamera(), { 100.0f, 100.0f }, { 100.0f, 100.0f }, RAYWHITE);
-        ctx->drawRectangle(getMainCamera(), { 200.0f, 200.0f }, { 100.0f, 100.0f }, RED);
-        ctx->drawRectangle(getMainCamera(), { 300.0f, 300.0f }, { 100.0f, 100.0f }, RED);
+        ctx->drawRectangle(getMainCamera(), { 200.0f, 200.0f }, { 100.0f, 100.0f }, MAGENTA);
+        ctx->drawRectangle(getMainCamera(), { 300.0f, 300.0f }, { 100.0f, 100.0f }, YELLOW);
     }
 
     aiko::AikoPtr<Mesh> RenderSystem::createMesh(Mesh::MeshType type)
