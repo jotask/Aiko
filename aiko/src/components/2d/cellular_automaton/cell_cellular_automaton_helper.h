5eab530936f47641fd515e0d9acb6de1d3a3da4c
Jose <j.iznardo@double11.com>
2024-09-21 21:32:49 +0100

    make more chunks and less size

--- aiko/src/components/2d/cellular_automaton/cell_cellular_automaton_helper.h
+++ aiko/src/components/2d/cellular_automaton/cell_cellular_automaton_helper.h
@@ -9,15 +9,16 @@ namespace aiko
     namespace cellautomaton
     {
 
-        constexpr const uint SIZE_WORLD = 2; // How many initial chunks
-        constexpr const uint SIZE_CHUNK = 64; // How many cells in a chunk
+        constexpr const uint SIZE_WORLD = 4; // How many initial chunks
+        constexpr const uint SIZE_CHUNK = 32; // How many cells in a chunk
 
         inline int getIndex(int x, int y, int width)
         {
             return width * x + y;
         }
 
-        inline int getChunkIndex(int x, int y, int width) {
+        inline int getChunkIndex(int x, int y, int width)
+        {
             return y * width + x;
         }
 
