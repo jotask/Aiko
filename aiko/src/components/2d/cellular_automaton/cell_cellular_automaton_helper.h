dd103c20bd268caaf76819bb2beadba09e989efa
Jose <j.iznardo@double11.com>
2024-09-21 21:05:19 +0100

    make index generator to inline resize world

--- aiko/src/components/2d/cellular_automaton/cell_cellular_automaton_helper.h
+++ aiko/src/components/2d/cellular_automaton/cell_cellular_automaton_helper.h
@@ -9,10 +9,10 @@ namespace aiko
     namespace cellautomaton
     {
 
-        constexpr const uint SIZE_WORLD = 4; // How many initial chunks
-        constexpr const uint SIZE_CHUNK = 32; // How many cells in a chunk
+        constexpr const uint SIZE_WORLD = 2; // How many initial chunks
+        constexpr const uint SIZE_CHUNK = 64; // How many cells in a chunk
 
-        static int getIndex(int x, int y, int width)
+        inline int getIndex(int x, int y, int width)
         {
             return width * x + y;
         }
