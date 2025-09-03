807f1aba4c51695838e2443e73a1dfcfc967de76
Jose <josevivesiznardo@gmail.com>
2025-09-03 19:51:53 +0200

    disable ASIO

--- examples/loop_recorder/Dependencies.cmake
+++ examples/loop_recorder/Dependencies.cmake
@@ -6,13 +6,15 @@ Set(FETCHCONTENT_QUIET FALSE)
 #----------------------------------------------------------------------
 
 # Fetch PortAudio
+
+set(PORTAUDIO_BUILD_ASIO OFF CACHE BOOL "Disable ASIO support" FORCE)
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build PortAudio as a static library" FORCE)
+
 FetchContent_Declare(
     PortAudio
     GIT_REPOSITORY https://github.com/PortAudio/portaudio.git
     GIT_TAG        v19.7.0  # Latest stable version
 )
-
-set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build PortAudio as a static library" FORCE)
 
 # Make available
 FetchContent_MakeAvailable(PortAudio)
