398f9269dc62120d4ffe79d7acba73b725337852
Jose <josevivesiznardo@gmail.com>
2025-09-12 00:00:13 +0200

    fix vertex attributes ordering

--- aiko/src/platform/bgfx/bgfx_render_utils.cpp
+++ aiko/src/platform/bgfx/bgfx_render_utils.cpp
@@ -15,8 +15,8 @@ namespace aiko
                 .begin()
                 .add(::bgfx::Attrib::Position, 3, ::bgfx::AttribType::Float)
                 .add(::bgfx::Attrib::TexCoord0, 2, ::bgfx::AttribType::Float)
-                .add(::bgfx::Attrib::Color0, 4, ::bgfx::AttribType::Uint8, true)
                 .add(::bgfx::Attrib::Normal, 3, ::bgfx::AttribType::Float, true, true)
+                .add(::bgfx::Attrib::Color0, 4, ::bgfx::AttribType::Uint8, true)
                 .end();
         }
 
