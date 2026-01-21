e0f17bcbc7d9ed44bb2deeb649c2aa7c68c56801
Jose <josevivesiznardo@gmail.com>
2026-01-21 22:57:47 +0100

    inline print node

--- examples/naiko/lib/compiler_helper.h
+++ examples/naiko/lib/compiler_helper.h
@@ -8,7 +8,7 @@
 namespace aiko::naiko
 {
 
-    void printToken(const Token token)
+    inline void printToken(const Token token)
     {
         logger::Log::info("Token: %s -> %s", magic_enum::enum_name(token.kind).data(), token.text.c_str());
     }
