1932b9ef0d25cc7d2044ad316311a25a42d70607
Jose <josevivesiznardo@gmail.com>
2026-01-21 22:59:17 +0100

    clean up main

--- examples/naiko/src/main.cpp
+++ examples/naiko/src/main.cpp
@@ -8,6 +8,8 @@
 #include <compiler_types.h>
 #include <compiler_helper.h>
 #include <lexer.h>
+#include <parser.h>
+#include <emitter.h>
 
 #include "constants.h"
 
@@ -18,15 +20,39 @@ int main()
 	const aiko::string fullPath = naiko::global::getAssetPath("naiko.naiko");
 	const aiko::string code = aiko::files::readFileContent(fullPath.c_str());
 
+	aiko::naiko::Tokenization tokenizator;
+
+	// ###########
+	//    LEXER
+	// ###########
+
 	aiko::naiko::Lexer lexer(code);
 
+	auto processToken = [&](const aiko::naiko::Token token)
+	{
+		tokenizator.push_back(token);
+		aiko::naiko::printToken(token);
+	};
+
 	aiko::naiko::Token token = lexer.next();
-	aiko::naiko::printToken(token);
+	processToken(token);
 	while (token.kind != aiko::naiko::TokenKind::END)
 	{
 		token = lexer.next();
-		aiko::naiko::printToken(token);
+		processToken(token);
 	}
+
+	// ############
+	//    PARSER
+	// ############
+
+	aiko::naiko::Parser emitter(tokenizator);
+
+	// ############
+	//    EMITTER
+	// ############
+
+	// TODO
 
 	return EXIT_SUCCESS;
 }
