0b11cbd5a673f404c40579be895ef402bedee30b
Jose <josevivesiznardo@gmail.com>
2026-01-23 19:31:20 +0100

    add helper function to make indent for logs

--- examples/naiko/lib/compiler_helper.h
+++ examples/naiko/lib/compiler_helper.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "compiler_types.h"
+#include "parser_nodes.h"
 
 #include <logger/logger.h>
 #include <magic_enum/magic_enum.hpp>
@@ -11,6 +12,12 @@ namespace aiko::naiko
     inline void printToken(const Token token)
     {
         logger::Log::info("Token: %s -> %s", magic_enum::enum_name(token.kind).data(), token.text.c_str());
+    }
+
+    inline string makeIndent(const size_t indent)
+    {
+        constexpr size_t spacesPerLevel = 2;
+        return string(indent * spacesPerLevel, ' ');
     }
 
 }
