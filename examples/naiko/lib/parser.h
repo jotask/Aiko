b1c4d39959648b9cce5fd1da40114031e049157e
Jose <josevivesiznardo@gmail.com>
2026-01-23 14:50:19 +0100

    parser add template function for helpers

--- examples/naiko/lib/parser.h
+++ examples/naiko/lib/parser.h
@@ -32,6 +32,12 @@ namespace aiko::naiko
 
         void statement();
 
+        template<class T>
+        bool checkCurrent(TokenKind, T) const;
+
+        template<class T>
+        bool isTokenMatch(Token token, TokenKind kind, T naiko) const;
+
         void processKeyword();
         void processComparison();
 
@@ -39,5 +45,53 @@ namespace aiko::naiko
         size_t m_current;
 
     };
+
+    template <class T>
+    bool Parser::checkCurrent(TokenKind kind, T naiko) const
+    {
+        const auto current = getCurrentToken();
+        if (current.kind != kind)
+        {
+            return false;
+        }
+        if (const T* item = std::get_if<T>(&current.naiko))
+        {
+            return *item == naiko;
+        }
+        return false;
+    }
+
+    template<class T>
+    bool Parser::isTokenMatch(Token token, TokenKind kind, T naiko) const
+    {
+        if (token.kind != kind)
+        {
+            return false;
+        }
+
+        if constexpr (std::is_same_v<T, NaikoKeyword>)
+        {
+            if (std::holds_alternative<NaikoKeyword>(token.naiko))
+            {
+
+                return std::get<NaikoKeyword>(token.naiko) == naiko;
+            }
+        }
+        else if constexpr (std::is_same_v<T, NaikoOperation>)
+        {
+            if (std::holds_alternative<NaikoOperation>(token.naiko))
+            {
+                return std::get<NaikoOperation>(token.naiko) == naiko;
+            }
+        }
+        else if constexpr (std::is_same_v<T, NaikoType>)
+        {
+            if (std::holds_alternative<NaikoType>(token.naiko))
+            {
+                return std::get<NaikoType>(token.naiko) == naiko;
+            }
+        }
+        return false;
+    }
 
 }
