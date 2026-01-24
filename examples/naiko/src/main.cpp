78af3514637db76b442e012837eea53c87cb05d3
Jose <josevivesiznardo@gmail.com>
2026-01-24 21:27:25 +0100

    main parse compiler with options args

--- examples/naiko/src/main.cpp
+++ examples/naiko/src/main.cpp
@@ -9,24 +9,31 @@
 #include <compiler_helper.h>
 #include <lexer.h>
 #include <parser.h>
-#include <emitter.h>
+#include <emitter/emitter.h>
+#include <naiko_compiler_options.h>
 
 #include "constants.h"
 
 #include <logger/logger.h>
 
-int main()
-{
-	const aiko::string fullPath = naiko::global::getAssetPath("naiko.naiko");
-	const aiko::string code = aiko::files::readFileContent(fullPath.c_str());
+#include "emitter/cpp/cpp_emitter.h"
 
-	aiko::naiko::Tokenization tokenizator;
+
+int main(int argc, char** argv)
+{
+
+	const aiko::naiko::CompilerOptions opts = aiko::naiko::parseArgs(argc, argv);
+
+	const aiko::string code = aiko::files::readFileContent(opts.inputFiles.front().c_str());
 
 	// ###########
 	//    LEXER
 	// ###########
 
+	aiko::logger::Log::info("Lexer Started");
+
 	aiko::naiko::Lexer lexer(code);
+	aiko::naiko::Tokenization tokenizator;
 
 	auto processToken = [&](const aiko::naiko::Token token)
 	{
@@ -42,17 +49,55 @@ int main()
 		processToken(token);
 	}
 
+	aiko::logger::Log::info("Lexer completed...");
+
 	// ############
 	//    PARSER
 	// ############
 
-	aiko::naiko::Parser emitter(tokenizator);
+	aiko::logger::Log::info("Parser Started");
+
+	aiko::naiko::Parser parser(tokenizator);
+	auto ast = parser.program();
+
+	aiko::logger::Log::info("Parser completed...");
 
 	// ############
 	//    EMITTER
 	// ############
 
-	// TODO
+	aiko::logger::Log::info("Emitter Started");
+
+	aiko::AikoUPtr<aiko::naiko::Emitter> emitter;
+
+	switch (opts.emitterKind)
+	{
+		case aiko::naiko::CompilerOptions::EmitterKind::CPP: emitter = std::make_unique<aiko::naiko::CppEmitter>(opts.outputFile); break;
+		default:
+			aiko::logger::Log::error("Unsuportted emitter mode");
+			std::exit(EXIT_FAILURE);
+	}
+
+	emitter->emit(ast.get());
+
+	aiko::logger::Log::info("Emitter completed...");
+
+	// ############
+	//    Cpp compile
+	// ############
+
+	{
+		const aiko::string cmd = "g++ " + opts.outputFile + ".cpp -o " + opts.outputFile;
+		int result = std::system(cmd.c_str());
+		if (result != EXIT_SUCCESS)
+		{
+			// On POSIX systems, exit code is in the high byte
+			const int exitCode = WEXITSTATUS(result);
+			aiko::logger::Log::info("Exit code: [%d]", exitCode);
+			aiko::logger::Log::critical("Couldn't compile emitted code exited with error code [%d] -> [%s] -> [%s]", exitCode, opts.outputFile.c_str(), cmd.c_str());
+		}
+
+	}
 
 	return EXIT_SUCCESS;
 }
