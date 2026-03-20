ee158d31f4c238461b3ca5a6fe1973c23577db23
Jose Vives <jota_sk@hotmail.com>
2026-03-20 10:45:14 +0100

    clean up cmake project set up

---
+++ cmake/ProjectOptions.cmake
@@ -0,0 +1,42 @@
+function(aiko_setup_options)
+    option(AIKO_ENABLE_UNITY "Enable unity/jumbo builds" OFF)
+    option(AIKO_BUILD_EXAMPLES "Build example executables" OFF)
+
+    set(AIKO_RENDER "AIKO_BGFX" CACHE STRING "Render backend")
+    set_property(CACHE AIKO_RENDER PROPERTY STRINGS AIKO_BGFX AIKO_NATIVE)
+
+    # Initialize unity build for targets created after this point
+    if(AIKO_ENABLE_UNITY)
+        set(CMAKE_UNITY_BUILD ON PARENT_SCOPE)
+    else()
+        set(CMAKE_UNITY_BUILD OFF PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(aiko_detect_platform)
+    if(WIN32)
+        set(AIKO_CURRENT_PLATFORM AIKO_WINDOWS PARENT_SCOPE)
+    elseif(UNIX)
+        set(AIKO_CURRENT_PLATFORM AIKO_LINUX PARENT_SCOPE)
+    else()
+        message(FATAL_ERROR "Unsupported OS")
+    endif()
+endfunction()
+
+function(aiko_apply_defaults target_name)
+
+    target_compile_features(${target_name} PUBLIC cxx_std_20)
+    target_compile_definitions(${target_name} PUBLIC NOMINMAX)
+
+    if(AIKO_CURRENT_PLATFORM)
+        target_compile_definitions(${target_name} PUBLIC ${AIKO_CURRENT_PLATFORM})
+    endif()
+
+    if(AIKO_ENABLE_UNITY)
+        set_target_properties(${target_name} PROPERTIES UNITY_BUILD ON)
+    endif()
+endfunction()
+
+function(aiko_set_common_source_layout target_name root_dir)
+    source_group(TREE ${root_dir} FILES ${ARGN})
+endfunction()
