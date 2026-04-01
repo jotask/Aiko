cc2e8bbf8e1648dfd028b53279a795c7901f948f
Jose Vives <jota_sk@hotmail.com>
2026-04-01 13:39:07 +0200

    add more tracy profiler functionality

--- aiko/core/src/intrumentor/profiler.h
+++ aiko/core/src/intrumentor/profiler.h
@@ -1,16 +1,21 @@
 #pragma once
 
+#include <logger/logger.h>
+
 #ifdef AIKO_PROFILER
     #include <source_location>
     #include <tracy/Tracy.hpp>
+    #include <common/TracyVersion.hpp>
 #endif
 
 #ifdef AIKO_PROFILER
     #define AIKO_ZONE_SCOPED ZoneScoped;
-    #define AIKO_ZONE_NAMED(name) ZoneScoped(name)
+    #define AIKO_ZONE_NAMED(name) ZoneScopedN(name)
     #define AIKO_FRAME_MARK FrameMark;
+    #define AIKO_PROFILER_VERSION_PRINT aiko::logger::Log::trace( "[TRACY] v%d.%d.%d", tracy::Version::Major, tracy::Version::Minor, tracy::Version::Patch);
 #else
     #define AIKO_ZONE_SCOPED
     #define AIKO_ZONE_NAMED(name)
     #define AIKO_FRAME_MARK
+    #define AIKO_PROFILER_VERSION_PRINT
 #endif
--- aiko/core/src/intrumentor/profiler_counter.h
+++ aiko/core/src/intrumentor/profiler_counter.h
@@ -2,49 +2,94 @@
 
 #include <atomic>
 #include <cstdint>
+#include <utility>
 
 namespace aiko::profile
 {
     struct CopyMoveCounters
     {
-        std::atomic<uint64_t> copyCtor{0};
-        std::atomic<uint64_t> moveCtor{0};
-        std::atomic<uint64_t> copyAssign{0};
-        std::atomic<uint64_t> moveAssign{0};
+        std::atomic<uint64_t> copyCtorCount{0};
+        std::atomic<uint64_t> moveCtorCount{0};
+        std::atomic<uint64_t> copyAssignCount{0};
+        std::atomic<uint64_t> moveAssignCount{0};
 
         void reset()
         {
-            copyCtor.store(0, std::memory_order_relaxed);
-            moveCtor.store(0, std::memory_order_relaxed);
-            copyAssign.store(0, std::memory_order_relaxed);
-            moveAssign.store(0, std::memory_order_relaxed);
+            copyCtorCount.store(0, std::memory_order_relaxed);
+            moveCtorCount.store(0, std::memory_order_relaxed);
+            copyAssignCount.store(0, std::memory_order_relaxed);
+            moveAssignCount.store(0, std::memory_order_relaxed);
         }
     };
-}
+
+    template<typename T>
+    struct CopyMoveTracked
+    {
+#if defined(AIKO_PROFILE_COPIES)
+        inline static CopyMoveCounters s_counters{};
+#endif
+
+        CopyMoveTracked() = default;
+
+        CopyMoveTracked(const CopyMoveTracked&)
+        {
+#if defined(AIKO_PROFILE_COPIES)
+            s_counters.copyCtorCount.fetch_add(1, std::memory_order_relaxed);
+#endif
+        }
+
+        CopyMoveTracked(CopyMoveTracked&&) noexcept
+        {
+#if defined(AIKO_PROFILE_COPIES)
+            s_counters.moveCtorCount.fetch_add(1, std::memory_order_relaxed);
+#endif
+        }
+
+        CopyMoveTracked& operator=(const CopyMoveTracked&)
+        {
+#if defined(AIKO_PROFILE_COPIES)
+            s_counters.copyAssignCount.fetch_add(1, std::memory_order_relaxed);
+#endif
+            return *this;
+        }
+
+        CopyMoveTracked& operator=(CopyMoveTracked&&) noexcept
+        {
+#if defined(AIKO_PROFILE_COPIES)
+            s_counters.moveAssignCount.fetch_add(1, std::memory_order_relaxed);
+#endif
+            return *this;
+        }
 
 #if defined(AIKO_PROFILE_COPIES)
-
-    #define AIKO_DECLARE_COPY_MOVE_COUNTERS() \
-        inline static aiko::profile::CopyMoveCounters s_counters{}; \
-        static void resetCounters() { s_counters.reset(); }
-
-    #define AIKO_COUNT_COPY_CTOR() \
-        s_counters.copyCtor.fetch_add(1, std::memory_order_relaxed)
-
-    #define AIKO_COUNT_MOVE_CTOR() \
-        s_counters.moveCtor.fetch_add(1, std::memory_order_relaxed)
-
-    #define AIKO_COUNT_COPY_ASSIGN() \
-        s_counters.copyAssign.fetch_add(1, std::memory_order_relaxed)
-
-    #define AIKO_COUNT_MOVE_ASSIGN() \
-        s_counters.moveAssign.fetch_add(1, std::memory_order_relaxed)
-
-#else
-
-#define AIKO_DECLARE_COPY_MOVE_COUNTERS()
-#define AIKO_COUNT_COPY_CTOR()
-#define AIKO_COUNT_MOVE_CTOR()
-#define AIKO_COUNT_COPY_ASSIGN()
-#define AIKO_COUNT_MOVE_ASSIGN()
+        static void resetCounters()
+        {
+            s_counters.reset();
+        }
 #endif
+    };
+
+    #if defined(AIKO_PROFILE_COPIES)
+        #define AIKO_PRINT_PROFILER_COUNTERS(classType)                                                         \
+        do                                                                                                      \
+        {                                                                                                       \
+            static float accum = 0.0f;                                                                          \
+            accum += aiko::Time::it().getDeltaTime();                                                           \
+            if (accum >= 1.0f)                                                                                  \
+            {                                                                                                   \
+                accum = 0.0f;                                                                                   \
+                logger::Log::info(                                                                              \
+                #classType " copies: ctor=%llu moveCtor=%llu copyAssign=%llu moveAssign=%llu",                  \
+                (unsigned long long)classType::s_counters.copyCtorCount.load(std::memory_order_relaxed),        \
+                (unsigned long long)classType::s_counters.moveCtorCount.load(std::memory_order_relaxed),        \
+                (unsigned long long)classType::s_counters.copyAssignCount.load(std::memory_order_relaxed),      \
+                (unsigned long long)classType::s_counters.moveAssignCount.load(std::memory_order_relaxed)       \
+                );                                                                                              \
+                classType::resetCounters();                                                                     \
+            }                                                                                                   \
+        } while (0)
+    #else
+        #define AIKO_PRINT_PROFILER_COUNTERS(classType)
+    #endif
+
+}
