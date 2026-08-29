71606a0124d4205cc10121a46c23b34c4af840c6
Jose Vives <jota_sk@hotmail.com>
2026-08-29 19:44:04 +0200

    centralize asset type traits

--- aiko/engine/src/systems/asset_system.h
+++ aiko/engine/src/systems/asset_system.h
@@ -2,10 +2,10 @@
 
 #include "systems/base_system.h"
 
-#include "assets/asset_type.h"
+#include "assets/asset_traits.h"
+#include "assets/types/mesh_asset.h"
 #include "assets/types/shader_asset.h"
-
-#include <type_traits>
+#include "assets/types/texture_asset.h"
 
 namespace aiko
 {
@@ -22,7 +22,7 @@ namespace aiko
         template<typename T>
         AssetId load(string_view source)
         {
-            return load(assetType<T>(), source);
+            return load(assetTypeOf<T>(), source);
         }
 
         AssetId create(const TextureAsset& asset);
@@ -40,38 +40,6 @@ namespace aiko
         virtual void connect(ModuleConnector*, SystemConnector*) override;
 
     private:
-
-        template<typename>
-        inline static constexpr bool ALWAYS_FALSE = false;
-
-        template<typename T>
-        static consteval AssetType assetType()
-        {
-            if constexpr (std::is_same_v<T, TextureAsset>)
-            {
-                return AssetType::Texture;
-            }
-            else if constexpr (std::is_same_v<T, MeshAsset>)
-            {
-                return AssetType::Mesh;
-            }
-            else if constexpr (std::is_same_v<T, ModelAsset>)
-            {
-                return AssetType::Model;
-            }
-            else if constexpr (std::is_same_v<T, ShaderAsset>)
-            {
-                return AssetType::Shader;
-            }
-            else if constexpr (std::is_same_v<T, ComputeShaderAsset>)
-            {
-                return AssetType::ComputeShader;
-            }
-            else
-            {
-                static_assert(ALWAYS_FALSE<T>, "Unsupported asset type");
-            }
-        }
 
         AssetId load(AssetType type, string_view source);
 
