960f3ea67348ef1398e03cefc486e34f032f24b3
Jose Vives <jota_sk@hotmail.com>
2026-09-07 08:53:34 +0200

    enable aiko debug only in debug builds

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -72,7 +72,7 @@ function(aiko_apply_defaults target_name)
         target_compile_definitions(${target_name} PRIVATE AIKO_PROFILE_COPIES)
     endif()
 
-    target_compile_definitions(${target_name} PRIVATE AIKO_DEBUG)
+    target_compile_definitions(${target_name} PRIVATE $<$<CONFIG:Debug>:AIKO_DEBUG>)
 
 endfunction()
 
